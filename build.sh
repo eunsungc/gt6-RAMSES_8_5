bd2e07a0d35fcccb29f24d4f0a33e5d1c3547679
eunsungc <eunsung.jung@gmail.com>
2015-09-23 11:28:32 -0500

    added --enable-udt optino

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./configure --with-udt --prefix=$1
+./configure --with-udt --enable-udt --prefix=$1
 vi Makefile
 find . -type f -name aclocal.m4 -exec touch {} \;
 find . -type f -name Makefile.in -exec touch {} \;
